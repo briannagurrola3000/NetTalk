9d0a379db6671bbb5b430622e4a705463e681b85
briannagurrola3000 <bagurrola16@gmail.com>
2025-04-15 11:25:38 -0600

    Add files via upload

---
+++ save_key.py
@@ -0,0 +1,9 @@
+# save_key.py
+from cryptography.fernet import Fernet
+
+key = Fernet.generate_key()
+with open("shared.key", "wb") as f:
+    f.write(key)
+
+print("🔑 Key generated and saved to shared.key")
+
